161d6baaa71b734392f9b1af939daa674b564359
Louie Kert Basay <louiebasay@gmail.com>
2024-12-19 01:24:03 +0800

    Initial commit to OneHubEngineering task

---
+++ src/main/java/com/example/OneHubEngineering/OneHubEngineeringApplication.java
@@ -0,0 +1,13 @@
+package com.example.OneHubEngineering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneHubEngineeringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneHubEngineeringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/OneHubEngineering/OneHubEngineeringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OneHubEngineering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneHubEngineeringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
